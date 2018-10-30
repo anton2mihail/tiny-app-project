b756c061f9142ddaeddd26f59a0bea24497549a0
Anton Mihail Lachmaniucu <antonmihail@gmail.com>
2018-10-30 19:23:56 -0400

    Authentication completed as well as styling!

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const bd = require("body-parser");
+const cookieParser = require("cookie-parser");
 const PORT = 8080; // default port 8080
 
 let urlDatabase = {
@@ -17,33 +18,57 @@ const generateRandomUrl = params => {
 
 app.use(bd.json());
 app.use(bd.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.redirect("/urls");
 });
 
 app.get("/login", (req, res) => {
-  res.render("urls_login");
+  res.render("urls_login", {
+    username: req.cookies["username"]
+  });
 });
 app.post("/login", (req, res) => {
-  res.send("Validating....");
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/login");
 });
 
 app.get("/urls", (req, res) => {
-  res.render("urls_index", {
-    urls: urlDatabase
-  });
+  if (req.cookies["username"]) {
+    res.render("urls_index", {
+      username: req.cookies["username"],
+      urls: urlDatabase,
+      newUrl: null
+    });
+  } else {
+    res.redirect("/login");
+  }
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  if (req.cookies["username"]) {
+    res.render("urls_index", {
+      username: req.cookies["username"],
+      urls: urlDatabase,
+      newUrl: "t"
+    });
+  } else {
+    res.redirect("/login");
+  }
 });
 
 app.post("/urls", (req, res) => {
   generateRandomUrl(req.body.longURL);
-  res.send("OK");
+  res.redirect("/urls");
 });
 
 app.post("/urls/:id/delete", (req, res) => {
@@ -52,12 +77,18 @@ app.post("/urls/:id/delete", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = {
-    shortURL: Object.keys(urlDatabase).find(el => el === req.params.shortURL),
-    longURL: urlDatabase[req.params.shortURL]
-  };
-  res.render("urls_show", templateVars);
+  if (req.cookies["username"]) {
+    let templateVars = {
+      username: req.cookies["username"],
+      shortURL: Object.keys(urlDatabase).find(el => el === req.params.shortURL),
+      longURL: urlDatabase[req.params.shortURL]
+    };
+    res.render("urls_show", templateVars);
+  } else {
+    res.redirect("/login");
+  }
 });
+
 app.post("/urls/:shortURL", (req, res) => {
   urlDatabase[req.params.shortURL] = req.body.newURL;
   res.redirect("/urls");
