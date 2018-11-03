21d22ec64a853d62135cccd8d268577f0098b22a
Anton Mihail Lachmaniucu <antonmihail@gmail.com>
2018-11-03 15:24:07 -0400

    Added multiple comments to the /urls route

--- routes/urls.js
+++ routes/urls.js
@@ -1,12 +1,19 @@
 const express = require("express");
+// This is needed by express to use these routes
 const router = express.Router();
+//These are the database method objects
 const { urls, users } = require("../dbs/db_crud");
+// Setting up environment variables
 require("dotenv").config();
+// if env variable exists use that, otherwise use 5000
 const PORT = process.env.PORT || 5000;
+// Url to prefix the short urls for the redirects
 const PUBLIC_URL = process.env.PUBLIC_URL || "http://localhost:" + PORT;
 
+// Get method on '/urls'
 router.get("/", (req, res) => {
-  if (req.session.isPopulated) {
+  // If there is a user session currently,and the user is registered then render the index otherwise redirect to the login page
+  if (req.session.user_id && users.isRegisteredUser(req.session.user_id)) {
     res.render("urls_index", {
       username: req.session.user_id,
       urls: urls.getUrlsPerUser(req.session.user_id),
@@ -18,13 +25,15 @@ router.get("/", (req, res) => {
   }
 });
 
+// Post method for the urls endpoint that creates a new url
 router.post("/", (req, res) => {
   if (req.body.url != "" && req.session.user_id) {
     urls.create(req.body.url, req.session.user_id);
   }
-  res.redirect("./");
+  res.redirect("/urls");
 });
 
+// Get method for '/urls/new' endpoint that displays a form for a user to enter a new url
 router.get("/new", (req, res) => {
   if (req.session.user_id) {
     res.render("urls_index", {
@@ -38,14 +47,18 @@ router.get("/new", (req, res) => {
   }
 });
 
+// DELETE method for the '/urls/shortURL' endpoint that basically deletes a url for a user
 router.delete("/:shortURL", (req, res) => {
+  // If the user owns the url then delete it
   if (users.hasUrl(req.session.user_id, req.params.shortURL)) {
     users.removeUrl(req.session.user_id, req.params.shortURL);
   }
   res.redirect("./");
 });
 
+// Get method for the '/urls/shortURL' endpoint that displays a page where a user can edit a url and update it
 router.get("/:shortURL", (req, res) => {
+  // This can only happen if the user owns the url otherwise they are redirected to the '/urls' endpoint
   if (
     req.session.user_id &&
     users.hasUrl(req.session.user_id, req.params.shortURL)
@@ -61,6 +74,7 @@ router.get("/:shortURL", (req, res) => {
   }
 });
 
+//Put method for the '/urls/shortURL' endpoint that updates a users url
 router.put("/:shortURL", (req, res) => {
   urls.update(req.params.shortURL, req.body.newURL);
   res.redirect("./");
