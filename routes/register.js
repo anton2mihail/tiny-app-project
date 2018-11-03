4f19059f286b8950e50f62521c50919c95ea8197
Anton Mihail Lachmaniucu <antonmihail@gmail.com>
2018-11-03 15:49:37 -0400

    Commented the register endpoint

--- routes/register.js
+++ routes/register.js
@@ -1,17 +1,25 @@
 const express = require("express");
+// This is needed by express to use these routes
 const router = express.Router();
+//These are the database method objects
 const { urls, users } = require("../dbs/db_crud");
 const { createUnique } = require("../methods/session_id");
+// Setting up environment variables
+require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 const PUBLIC_URL = process.env.PUBLIC_URL || "http://localhost:" + PORT;
 
+// Post method for '/register endpoint'
 router.post("/", (req, res) => {
+  //Checks to make sure that user is not already registered
   let resp = users.isRegisteredUser(req.body.username);
   if (resp.username) {
     res.send("User email already in use. Please try again, or login");
   } else {
     users.create(req.body.username, req.body.password);
+    //Set the session cookies for each user
     req.session.session_id = createUnique();
+    // Sets a session cookie for the user_id (username)
     req.session.user_id = req.body.username;
     res.redirect("./urls");
   }
