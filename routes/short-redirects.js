368295d8b8ae3ee2e0c4e08bacb9b0baba0fd246
Anton Mihail Lachmaniucu <antonmihail@gmail.com>
2018-11-03 15:52:16 -0400

    Commented the go endpoint

--- routes/short-redirects.js
+++ routes/short-redirects.js
@@ -1,7 +1,11 @@
 const express = require("express");
+// This is needed by express to use these routes
 const router = express.Router();
+//These are the database method objects
 const { urls, users } = require("../dbs/db_crud");
 
+//Get method for the '/go/id' endpoint which redirects someone who has used the short urls to the
+// actual url that was initially shortened
 router.get("/:id", (req, res) => {
   let redirect = urls.findUrl(req.params.id);
   res.redirect(redirect);
